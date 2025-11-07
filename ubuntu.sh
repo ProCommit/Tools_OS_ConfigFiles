ccb609e55a6dbc5fcdcafb24fbfd3a27e6e63e76
ProCommit <oronhadad96@gmail.com>
2025-11-07 15:45:19 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,3 +2,4 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install build-essential gcc nasm gdb make curl wget git vim  cmake g++ pkg-config libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3  -y
 sudo snap install alacritty --classic
 sudo apt install neofetch eza -y
+󱆃
